85afaef1a92cacd6abede2ebd027e1bc8517663a
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2025-02-10 21:46:21 +0300

    query parametters

--- myapi.py
+++ myapi.py
@@ -1,4 +1,5 @@
 from fastapi  import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -20,3 +21,10 @@ def index():
 @app.get("/get_student/{student_id}")
 def get_student(student_id: int):
     return students[student_id]
+
+@app.get("/get-by-name")
+def get_student(*,name: Optional[str]=None, test : int):
+    for student_id in students:
+        if students[student_id]["name"]==name:
+            return students[student_id]
+    return {"Data":"Not found"}
